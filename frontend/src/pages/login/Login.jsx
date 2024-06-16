f45c10e2c545654bf63e425933a8296e56ae0ad4
Naveen <104302397+naikNaveen757@users.noreply.github.com>
2024-06-16 17:05:50 +0530

    login page init

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className=' flex flex-col items-center justify-center min-w-96 mx-auto'>
+    <div className=' w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    <h1 className=' text-3x1 font-semibold text-center text-gray-300'>login</h1>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Login
